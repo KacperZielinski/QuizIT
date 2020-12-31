467b6f0b142c843fec43c81c399040dd75a168a2
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-12-31 16:37:55 +0100

    Ugly way to pretty print info about backend response.

--- frontend-angular/src/app/component/simple-quiz/simple-quiz.component.ts
+++ frontend-angular/src/app/component/simple-quiz/simple-quiz.component.ts
@@ -10,13 +10,18 @@ import {WordService} from "../../service/word.service";
 export class SimpleQuizComponent implements OnInit {
 
   quizQuestions: Word[] = [];
+  isLoaded: boolean;
 
   constructor(private wordService: WordService) { }
 
   ngOnInit(): void {
     this.wordService.getQuizQuestions().subscribe(value => {
         this.quizQuestions = value;
+        this.isLoaded = true;
         console.log(value);
+    }, error => {
+      this.isLoaded = true;
+      console.log("Backend is offline..")
     });
   }
 
