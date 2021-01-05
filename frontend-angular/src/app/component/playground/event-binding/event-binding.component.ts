a3a66a498be19a2a40f2e3c0c58088d48556cfa5
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-01-06 00:58:39 +0100

    ngStyle directive, [] means ngStyle directive with property binding!

--- frontend-angular/src/app/component/playground/event-binding/event-binding.component.ts
+++ frontend-angular/src/app/component/playground/event-binding/event-binding.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-binding.component.scss']
 })
 export class EventBindingComponent implements OnInit {
-  result: string = 'pap';
+  result: string = '';
+  color: string = 'red';
 
   constructor() { }
 
@@ -14,9 +15,17 @@ export class EventBindingComponent implements OnInit {
     setTimeout(() => {
       this.result = 'Bingo!';
     }, 5000)
+
+    setTimeout(() => {
+      this.color = 'green';
+    }, 2000)
   }
 
   execute($event: Event) {
     this.result = (<HTMLInputElement>$event.target).value
+  }
+
+  getColor(): string {
+    return this.color;
   }
 }
